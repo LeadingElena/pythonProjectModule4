36c115e5cb959a9f1b2e642cc2e55c96f15d0697
DeveloperElena <elenafinansi80@mail.ru>
2024-11-25 12:31:48 +0700

    Add homework Modules and packages

---
+++ Modules_and_packages/fake_math.py
@@ -0,0 +1,5 @@
+def divide (num1, num2):
+    if num2:
+        return num1 / num2
+    else:
+        return 'Ошибка'
---
+++ Modules_and_packages/true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(num1, num2):
+    if num2:
+        return num1 / num2
+    else:
+        return inf
